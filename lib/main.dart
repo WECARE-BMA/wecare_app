a626e0b71e75320590c516fe2e03899aab6a776e
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-04 13:00:21 +0300

    adjusted the color theme - see WEC-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final MaterialColor myCustomColor = MaterialColor(0xFFADE25D, {
+    50: Color(0xFFF6FFE1),
+    100: Color(0xFFE9FFA6),
+    200: Color(0xFFDAFF6B),
+    300: Color(0xFFCCFF30),
+    400: Color(0xFFC2FF0A),
+    500: Color(0xFFADE25D),
+    600: Color(0xFF9BD055),
+    700: Color(0xFF86BA4C),
+    800: Color(0xFF72A842),
+    900: Color(0xFF578B32),
+  });
 
   // This widget is the root of your application.
   @override
@@ -13,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primaryColor: Colors.blue,
+          primarySwatch: myCustomColor,
         ),
         home: const SigninPage());
   }
@@ -76,28 +89,40 @@ class _SigninFormState extends State<SigninForm> {
           Padding(
             padding: const EdgeInsets.only(bottom: 18),
             child: TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your email',
+              decoration: InputDecoration(
+                  floatingLabelBehavior: FloatingLabelBehavior.never,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  contentPadding:
+                      EdgeInsets.only(top: 20, bottom: 18, left: 10, right: 10),
+                  // hintText: 'Enter your email',
                   labelText: 'Email'),
             ),
           ),
           Padding(
             padding: const EdgeInsets.only(bottom: 18),
             child: TextFormField(
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your password',
-                  labelText: 'Password'),
+              decoration: InputDecoration(
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                contentPadding:
+                    EdgeInsets.only(top: 20, bottom: 18, left: 10, right: 10),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                // hintText: 'Enter your password',
+                labelText: 'Password',
+              ),
             ),
           ),
-          Container(
+          SizedBox(
             width: double.infinity,
+            height: 55,
             child: TextButton(
               onPressed: () {},
               child: Text('Sign In'),
               style: TextButton.styleFrom(
-                backgroundColor: Colors.green,
+                backgroundColor: Theme.of(context).primaryColor,
                 foregroundColor: Colors.white,
                 elevation: 0,
               ),
