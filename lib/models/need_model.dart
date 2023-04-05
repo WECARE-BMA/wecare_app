34f8fb45e6a572af7e28e50fdb12cef6d97ee02c
hemensol <hemensol14@yahoo.com>
2023-04-05 10:03:36 +0300

    History page initial commit and model

---
+++ lib/models/need_model.dart
@@ -0,0 +1,13 @@
+class Need {
+  final int id;
+  final String name;
+  final int amount;
+  final String image;
+  
+  Need({
+    required this.id,
+    required this.name,
+    required this.amount,
+    required this.image,
+  });
+}
