5f86156824918023c9a982e2a802af9533c9c015
Ama003 <amanuelademe2@gmail.com>
2023-04-05 12:02:32 +0300

    created nav bar - WEC-2

---
+++ lib/components/top_nav.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class TopNav extends StatelessWidget {
+  const TopNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.only(right: 20, left: 20),
+        height: 90,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/nav_background.png'),
+              fit: BoxFit.cover,
+              alignment: Alignment.topLeft),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset('assets/images/nav_logo.png'),
+            Row(
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Code to execute when the button is pressed
+                  },
+                  icon: const Icon(
+                    Icons.bookmark_outline,
+                    size: 30,
+                  ),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Theme.of(context).primaryColor),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.transparent),
+                    elevation: const MaterialStatePropertyAll(0),
+                  ),
+                  label: const Text(''),
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    // Code to execute when the button is pressed
+                  },
+                  icon: const Icon(
+                    Icons.notifications_none_outlined,
+                    size: 30,
+                  ),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                        Theme.of(context).primaryColor),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.transparent),
+                    elevation: const MaterialStatePropertyAll(0),
+                  ),
+                  label: const Text(''),
+                ),
+              ],
+            ),
+          ],
+        ) // Your content goes here
+
+        );
+  }
+}
