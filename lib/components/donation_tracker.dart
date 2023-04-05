c6e8f9529d899717c839cca4a817b1090441c786
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-05 13:14:20 +0300

    fixed issues in the tracker - see WEC-8

--- lib/components/donation_tracker.dart
+++ lib/components/donation_tracker.dart
@@ -17,57 +17,60 @@ class _DonationTrackerState extends State<DonationTracker> {
   Widget build(BuildContext context) {
     double progress_value = currently_collected / full_amount;
 
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          height: 60,
-          padding: EdgeInsets.only(left: 20, right: 20),
-          width: double.infinity,
-          child: Column(
+    return Container(
+      height: 60,
+      padding: EdgeInsets.only(left: 20, right: 20),
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                '\$$currently_collected ',
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15),
+              ),
+              Text(
+                'fund raised from \$$full_amount',
+                style: TextStyle(color: Color(0xff787878)),
+              )
+            ],
+          ),
+          LinearProgressIndicator(
+            value: progress_value,
+            backgroundColor: Color.fromARGB(255, 230, 230, 230),
+          ),
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
                 children: [
+                  Text('$donors ',
+                      style: TextStyle(color: Theme.of(context).primaryColor)),
                   Text(
-                    '\$$currently_collected ',
-                    style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 15),
-                  ),
-                  Text(
-                    'fund raised from \$$full_amount',
+                    'Donator\'s',
                     style: TextStyle(color: Color(0xff787878)),
-                  )
+                  ),
                 ],
               ),
-              LinearProgressIndicator(
-                value: progress_value,
-                backgroundColor: Color.fromARGB(255, 230, 230, 230),
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
-                    children: [
-                      Text('$donors ',
-                          style:
-                              TextStyle(color: Theme.of(context).primaryColor)),
-                      Text(
-                        'Donator\'s',
-                        style: TextStyle(color: Color(0xff787878)),
-                      ),
-                    ],
+                  Text(
+                    '$days',
+                    style: TextStyle(color: Theme.of(context).primaryColor),
                   ),
                   Text(
-                    '$days day\’s left',
+                    ' day\’s left',
                     style: TextStyle(color: Color(0xff787878)),
-                  )
+                  ),
                 ],
               )
             ],
-          ),
-        ),
+          )
+        ],
       ),
     );
   }
