437013b39945c21bc45bb3176d6b6b1911a03751
hemensol <hemensol14@yahoo.com>
2023-04-06 15:00:37 +0300

    added top navigation

--- lib/views/history_page.dart
+++ lib/views/history_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wecare_app/components/kid_tile.dart';
+import 'package:wecare_app/components/top_nav.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
@@ -7,36 +8,33 @@ class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(left: 10.0, bottom: 10.0),
-                child: Text(
-                  "History of your donations",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24
-                    )
-                ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const TopNav(),
+            const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Text(
+                "History of your donations",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24
+                )
               ),
-              Expanded(child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return KidTile(
-                    name: "Abel Mekonene", 
-                    image: "https://nationaltoday.com/wp-content/uploads/2021/04/Every-Kid-Healthy.jpg", 
-                    age: 12, 
-                    description: "Lorem Ipsum blah blah some stuff just talking, also very good student we love homeboy"
-                  );
-                }
-              )
-            )],
-          ),
+            ),
+            Expanded(child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return KidTile(
+                  name: "Abel Mekonene", 
+                  image: "https://nationaltoday.com/wp-content/uploads/2021/04/Every-Kid-Healthy.jpg", 
+                  age: 12, 
+                  description: "Lorem Ipsum blah blah some stuff just talking, also very good student we love homeboy"
+                );
+              }
+            )
+          )],
         ),
       ),
     );
