a23ee8b1d2afe4097be1ce604a7845340a2f5111
hawani99 <hawibek223@gmail.com>
2023-04-13 10:16:27 +0300

    donation bloc done WEC-12

--- lib/bloc/donation_bloc/donation_bloc.dart
+++ lib/bloc/donation_bloc/donation_bloc.dart
@@ -1,13 +1,36 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'donation_event.dart';
 part 'donation_state.dart';
 
-class DonationBloc extends Bloc<DonationEvent, DonationState> {
-  DonationBloc() : super(DonationInitial()) {
-    on<DonationEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+class DonationBloc {
+  int _donationAmount = 0;
+  final _donationStateController = StreamController<DonationState>();
+
+  Stream<DonationState> get donationStateStream =>
+      _donationStateController.stream;
+
+  void _emitDonationState() {
+    _donationStateController.sink
+        .add(DonationUpdatedState(donationAmount: _donationAmount));
+  }
+
+  void handleDonationEvent(DonationEvent event) {
+    if (event is IncrementDonationEvent) {
+      _donationAmount += 10;
+      _emitDonationState();
+    } else if (event is DecrementDonationEvent) {
+      if (_donationAmount > 0) {
+        _donationAmount -= 10;
+        _emitDonationState();
+      } else if (event is SubmitDonationEvent) {}
+    }
+
+    void dispose() {
+      _donationStateController.close();
+    }
   }
 }
