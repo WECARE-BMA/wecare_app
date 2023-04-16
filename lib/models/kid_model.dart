c078e594e1ba857f5262a12f42e4fe3bef94b4d5
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-16 13:17:24 +0300

    updated kids model - see WEC-17

--- lib/models/kid_model.dart
+++ lib/models/kid_model.dart
@@ -1,12 +1,12 @@
 import 'package:wecare_app/models/need_model.dart';
 
 class Kid {
-  final int id;
+  final String id;
   final String name;
   final int age;
   final String description;
   final String image;
-  final List<Need> needs;  
+  final List<Need> needs;
 
   Kid({
     required this.id,
@@ -18,23 +18,38 @@ class Kid {
   });
 
   factory Kid.fromJson(Map<String, dynamic> parsedJson) {
-    final _needs = parsedJson['needs'].map((e) => Need(id: e['id'], name: e['name'], amount: e['amount'], image: e['image'], donor: e['donor']));
     return Kid(
       id: parsedJson['id'],
       name: parsedJson['name'],
       age: parsedJson['age'],
       description: parsedJson['description'],
-      image: parsedJson['image'],
-      needs: _needs
+      image: parsedJson.containsKey('image')
+          ? parsedJson['image']
+          : parsedJson['imageUrl'],
+      needs: (parsedJson['needs'] as List)
+          .map((needJson) => Need.fromJson(needJson))
+          .toList(),
     );
   }
 
-  static List KidList(List kids) {
-  List parsedKids = [];
-  for (var i = 0; i < kids.length; i++) {
-    parsedKids.add(Kid.fromJson(kids[i]));
+  toJson() {
+    Map<String, dynamic> json = {
+      'name': name,
+      'age': age,
+      'description': description,
+      'image': image,
+      'needs': needs.map((e) => "needs/${e.id}")
+    };
+
+    return json;
   }
-  return parsedKids;
+
+  static List<Kid>? kidList(List kids) {
+    List<Kid> parsedKids = [];
+    for (var i = 0; i < kids.length; i++) {
+      parsedKids.add(Kid.fromJson(kids[i]));
+    }
+    return parsedKids;
   }
 
   static void add(Kid kid) {}
