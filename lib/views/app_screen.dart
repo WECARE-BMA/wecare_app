2e4cd1995bd598920971cdfbd6e37c3a03c4c1d7
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-17 16:11:37 +0300

    updated the nav with bloc

--- lib/views/app_screen.dart
+++ lib/views/app_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:wecare_app/blocs/nav_bloc/nav_bloc_bloc.dart';
 import 'package:wecare_app/views/history_page.dart';
 import 'package:wecare_app/views/home_page.dart';
 import 'package:wecare_app/views/profile_page.dart';
@@ -17,14 +19,31 @@ class _AppScreenState extends State<AppScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      body: BlocBuilder<NavBloc, NavState>(
+        builder: (context, state) {
+          if (state is PageLoading) {
+            return Center(
+              child: CircularProgressIndicator(
+                  color: Theme.of(context).primaryColor),
+            );
+          } else if (state is HomePageLoaded) {
+            return HomePage();
+          } else if (state is ProfilePageLoaded) {
+            return ProfilePage();
+          } else if (state is HistroyPageLoaded) {
+            return HistoryPage();
+          }
+
+          return Container();
+        },
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
+          (index) => print(index);
+          context.read<NavBloc>().add(ClickedPageButton(index: index));
         },
+        selectedItemColor: Theme.of(context).primaryColor,
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.history),
