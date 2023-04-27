affbd14f57b0eb3b990ac03c32d4c659ab613e8c
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-27 15:55:52 +0300

    removed thirdparty auths

--- lib/views/auth_pages/third_party_auth.dart
+++ lib/views/auth_pages/third_party_auth.dart
@@ -5,65 +5,40 @@ class ThirdPartyAuths extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 20, top: 10),
-          child: Text(
-            'or continue with',
-            style: TextStyle(color: Color(0xff787878)),
+    return Container(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20, top: 10),
+            child: Text(
+              'or continue with',
+              style: TextStyle(color: Color(0xff787878)),
+            ),
           ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                backgroundImage: AssetImage('assets/images/google_logo.png'),
-                radius: 25,
-              ),
-            ),
-            SizedBox(
-              width: 35,
-            ),
-            GestureDetector(
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                backgroundImage: AssetImage('assets/images/fb_logo.png'),
-                radius: 25,
-              ),
-            ),
-            SizedBox(
-              width: 35,
-            ),
-            GestureDetector(
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                backgroundImage: AssetImage(
-                  'assets/images/apple_logo.png',
+          FilledButton(
+              onPressed: () {},
+              child: Container(
+                child: Row(
+                  children: [Image.asset('assets/images/google_logo.png')],
                 ),
-                radius: 25,
-              ),
+              )),
+          Container(
+            padding: const EdgeInsets.only(top: 6, bottom: 20),
+            width: double.infinity,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    child: Text('Have an account?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Color(0xff787878))),
+                    onPressed: () =>
+                        Navigator.popAndPushNamed(context, '/signInPage')),
+              ],
             ),
-          ],
-        ),
-        Container(
-          padding: const EdgeInsets.only(top: 6, bottom: 20),
-          width: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                  child: Text('Have an account?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Color(0xff787878))),
-                  onPressed: () =>
-                      Navigator.popAndPushNamed(context, '/signInPage')),
-            ],
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
