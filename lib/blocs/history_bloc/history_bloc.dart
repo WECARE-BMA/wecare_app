260b82de97c40dea47d013270bbda9c296b7fa01
Eyosiyas Mekbib <eyosi9000@gmail.com>
2023-04-27 18:02:00 +0300

    Added get kids from local db - see WEC-11

--- lib/blocs/history_bloc/history_bloc.dart
+++ lib/blocs/history_bloc/history_bloc.dart
@@ -1,10 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:wecare_app/blocs/history_bloc/history_event.dart';
 import 'package:wecare_app/blocs/history_bloc/history_state.dart';
+import 'package:wecare_app/models/donor_model.dart';
+import 'package:wecare_app/service/donorLocalDBService.dart';
+import 'package:wecare_app/service/kidLocalDBService.dart';
 import 'package:wecare_app/service/kidsApiService.dart';
 
 class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
   final _kidsServiceProvider = KidsServiceProvider();
+  final KidDBService kidDBService = KidDBService();
+  final DonorDBService donorDBService = DonorDBService();
+
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
   List kidsList = [];
   List needList = [];
 
@@ -21,5 +30,20 @@ class HistoryBloc extends Bloc<HistoryEvent, HistoryState> {
 
       emit(HistorySuccessState(KidL: kidsList));
     });
+
+    on<GetKids>(((event, emit) async {
+      emit(HistoryLoadingState());
+      final donors = await donorDBService.getDonors();
+
+      late Donor donor;
+
+      for (var d in donors) {
+        if (d.id == currentUser!.uid) {
+          donor = d;
+        }
+      }
+
+      kidsList = donor.kids!;
+    }));
   }
 }
